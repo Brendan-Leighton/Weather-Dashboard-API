c203b0649869a202f412925ac7bae22e330209f6
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-24 21:49:35 -0400

    Added HTML boiler, some notes on API call in JS

---
+++ app.js
@@ -0,0 +1,14 @@
+$('document').ready(function () {
+// api key 93d2bcd8f73ead9c329f7c0c19abca09
+
+// by latitude and longitude 
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
+
+// by city and state
+// api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}
+
+
+
+
+
+});
