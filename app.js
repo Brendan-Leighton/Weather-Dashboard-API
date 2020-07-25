fa5d1ed4ab6a3e8ddbd01e040d718067e1b1a397
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-24 22:42:58 -0400

    Added the basic HTML skeleton for the page, have some wierd overlapping happening

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ $('document').ready(function () {
 // by city and state
 // api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}
 
+function apiCall (city, state){
+    const queryURL = `api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=93d2bcd8f73ead9c329f7c0c19abca09`;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response);
+      });
+}
+
+apiCall('concord', 'nh');
 
 
 
