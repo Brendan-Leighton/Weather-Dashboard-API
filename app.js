7c5a1a93ca6b5a1d9d6ce81f61cd7ea05e21ec93
BR3NDAN <br3ndan.l8n@gmail.com>
2020-08-05 02:51:07 -0400

    added Vars for html elements. Changed color scheme. Added API Stuff folder.

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 $('document').ready(function () {
-// api key 93d2bcd8f73ead9c329f7c0c19abca09
+    const inputCityName = $('#city-search-input');
+    const inputStateName = $('#state-search-input');
+    const searchHistorySection = $('#search-history-section');
+    const cityDateHeader = $('#city-date-title-header');
+    const todaysWeatherStatsDisplay = $('#today-weather-stats-display');
+    const fiveDayForecastSection = $('#five-day-forecast');
+    const fiveDayDivIteration = '#day-div-';  // add ${i} to end of this when iterating the 5 day forecast 
 
-// by latitude and longitude 
-// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 
-// by city and state
-// api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}
+    // api key 93d2bcd8f73ead9c329f7c0c19abca09
 
-function apiCall (city, state){
-    const queryURL = `api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=93d2bcd8f73ead9c329f7c0c19abca09`;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-          console.log(response);
-      });
-}
+    // by latitude and longitude 
+    // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 
-apiCall('concord', 'nh');
+    // by city and state
+    // api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}
+
+    const weatherAPI = {
+        call() {
+            queryURL = 'api.openweathermap.org/data/2.5/weather?q=London,uk&appid=fe0750260bcd9b74a10be4cdcee06e63';
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+            });
+        }
+    }
+
+    weatherAPI.call();
 
 
 
